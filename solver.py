89a82c331c769edc95b5e4041c0319cc392096d9
Rakin Rahman <rakinhacker@gmail.com>
2025-12-15 23:08:48 +0600

    feat: add support for final exam modules

--- solver.py
+++ solver.py
@@ -21,7 +21,7 @@ bundle_dir = pathlib.Path(bundle_dir).as_posix()
 
 SUBMIT_ASSESSMENT_IMG = f"{bundle_dir}/assets/submit-assessment.png"
 QUESTION_IMG = f"{bundle_dir}/assets/question.png"
-MAX_EXAMS = 33
+MAX_EXAMS = 35
 
 curr_exam = 1
 
@@ -30,45 +30,50 @@ def get_soup() -> BeautifulSoup:
     url = ""
 
     match curr_exam:
-        # Checkpoint exams
+        # Final exam modules
         case 15:
-            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-1-chapters-1-4-answers.html"
+            url = "https://itexamanswers.net/it-essentials-7-0-final-exam-chapters-1-9-answers-full.html"
         case 16:
-            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-2-chapters-5-6-answers.html"
+            url = "https://itexamanswers.net/it-essentials-7-0-final-exam-chapters-10-14-answers-full.html"
+        # Checkpoint exams
         case 17:
-            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-3-chapters-7-8-answers.html"
+            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-1-chapters-1-4-answers.html"
         case 18:
-            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-4-chapters-10-11-answers.html"
+            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-2-chapters-5-6-answers.html"
         case 19:
+            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-3-chapters-7-8-answers.html"
+        case 20:
+            url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-4-chapters-10-11-answers.html"
+        case 21:
             url = "https://itexamanswers.net/ite-8-0-certification-checkpoint-exam-5-chapters-12-13-answers.html"
         # Summary quizzes
-        case 20:
-            url = "https://itexamanswers.net/it-essentials-8-module-1-quiz-answers-introduction-to-personal-computer-hardware.html"
-        case 21:
-            url = "https://itexamanswers.net/it-essentials-8-module-2-quiz-answers-pc-assembly.html"
         case 22:
-            url = "https://itexamanswers.net/it-essentials-8-module-3-quiz-answers-advanced-computer-hardware-quiz-answers.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-1-quiz-answers-introduction-to-personal-computer-hardware.html"
         case 23:
-            url = "https://itexamanswers.net/it-essentials-8-module-4-quiz-answers-preventive-maintenance-and-troubleshooting.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-2-quiz-answers-pc-assembly.html"
         case 24:
-            url = "https://itexamanswers.net/it-essentials-8-module-5-quiz-answers-networking-concepts.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-3-quiz-answers-advanced-computer-hardware-quiz-answers.html"
         case 25:
-            url = "https://itexamanswers.net/it-essentials-8-module-6-quiz-answers-applied-networking.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-4-quiz-answers-preventive-maintenance-and-troubleshooting.html"
         case 26:
-            url = "https://itexamanswers.net/it-essentials-8-module-7-quiz-answers-laptops-and-other-mobile-devices.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-5-quiz-answers-networking-concepts.html"
         case 27:
-            url = "https://itexamanswers.net/it-essentials-8-module-8-quiz-answers-printers.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-6-quiz-answers-applied-networking.html"
         case 28:
-            url = "https://itexamanswers.net/it-essentials-v7-01-chapter-9-quiz-answers.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-7-quiz-answers-laptops-and-other-mobile-devices.html"
         case 29:
-            url = "https://itexamanswers.net/it-essentials-8-module-10-quiz-answers-windows-installation.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-8-quiz-answers-printers.html"
         case 30:
-            url = "https://itexamanswers.net/it-essentials-8-module-11-quiz-answers-windows-configuration.html"
+            url = "https://itexamanswers.net/it-essentials-v7-01-chapter-9-quiz-answers.html"
         case 31:
-            url = "https://itexamanswers.net/it-essentials-8-module-12-quiz-answers-mobile-linux-and-macos-operating-systems.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-10-quiz-answers-windows-installation.html"
         case 32:
-            url = "https://itexamanswers.net/it-essentials-8-module-13-quiz-answers-security.html"
+            url = "https://itexamanswers.net/it-essentials-8-module-11-quiz-answers-windows-configuration.html"
         case 33:
+            url = "https://itexamanswers.net/it-essentials-8-module-12-quiz-answers-mobile-linux-and-macos-operating-systems.html"
+        case 34:
+            url = "https://itexamanswers.net/it-essentials-8-module-13-quiz-answers-security.html"
+        case 35:
             url = "https://itexamanswers.net/it-essentials-8-module-14-quiz-answers-the-it-professional.html"
         # Module exam
         case _:
